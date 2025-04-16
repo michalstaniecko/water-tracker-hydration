e1a9d928179b430aa313e0bdf45bbc0b2f1a7ac8
Michal Staniecko <michal.staniecko@gmail.com>
2025-04-16 22:12:51 +0200

    refresh date on app become active

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,36 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "react-native-reanimated";
 import "../global.css";
 import { useWaterStore } from "@/stores/water";
+import { AppState } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const appState = useRef(AppState.currentState);
   const { fetchOrInitData } = useWaterStore();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === "active"
+      ) {
+        fetchOrInitData();
+      }
+      appState.current = nextAppState;
+    });
     fetchOrInitData();
+    return () => {
+      subscription.remove();
+    };
   }, []);
 
   useEffect(() => {
