9ce15440e26e3b971307a2c81840b9303c8200e8
Michal Staniecko <michal.staniecko@gmail.com>
2025-04-18 14:57:23 +0200

    added tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from "expo-router";
 import React from "react";
 import { getToday } from "@/utils/date";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function TabLayout() {
   return (
@@ -14,18 +15,27 @@ export default function TabLayout() {
         options={{
           title: `Today: ${getToday()}`,
           tabBarLabel: "Today",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name={"home"} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name={"history"}
         options={{
           title: "History",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={24} name={"calendar"} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name={"setup"}
         options={{
           title: "Setup",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name={"cog"} color={color} />
+          ),
         }}
       />
     </Tabs>
