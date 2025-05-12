02ce4204b9a3f474af57370651e261411b7a604d
Michal Staniecko <michal.staniecko@gmail.com>
2025-05-12 22:45:21 +0200

    added ios baner

--- components/ads/Banner.tsx
+++ components/ads/Banner.tsx
@@ -5,11 +5,17 @@ import {
   useForeground,
 } from "react-native-google-mobile-ads";
 import React, { useRef } from "react";
-import { Platform } from "react-native";
+import {Platform} from "react-native";
+
+const ids = {
+  android: "ca-app-pub-7007354971618918/3882663398",
+  ios: "ca-app-pub-7007354971618918/3005971080",
+  default: TestIds.BANNER
+};
 
 const adUnitId = __DEV__
-  ? TestIds.ADAPTIVE_BANNER
-  : "ca-app-pub-7007354971618918/3882663398";
+  ? TestIds.BANNER
+  : Platform.select(ids);
 
 export const Banner = () => {
   const bannerRef = useRef<BannerAd>(null);
