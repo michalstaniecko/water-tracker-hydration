72cc79966df01004616380a2c8b88cd39b2ea0ad
Michal Staniecko <michal.staniecko@gmail.com>
2025-05-13 15:41:07 +0200

    eslint formating

--- components/ads/Banner.tsx
+++ components/ads/Banner.tsx
@@ -5,17 +5,15 @@ import {
   useForeground,
 } from "react-native-google-mobile-ads";
 import React, { useRef } from "react";
-import {Platform} from "react-native";
+import { Platform } from "react-native";
 
 const ids = {
   android: "ca-app-pub-7007354971618918/3882663398",
   ios: "ca-app-pub-7007354971618918/3005971080",
-  default: TestIds.BANNER
+  default: TestIds.BANNER,
 };
 
-const adUnitId = __DEV__
-  ? TestIds.BANNER
-  : Platform.select(ids);
+const adUnitId = __DEV__ ? TestIds.BANNER : Platform.select(ids);
 
 export const Banner = () => {
   const bannerRef = useRef<BannerAd>(null);
