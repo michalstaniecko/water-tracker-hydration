e38b17e7f10cddf57fd9af314dc5b31aba2c129b
Michal Staniecko <michal.staniecko@gmail.com>
2025-05-22 19:54:37 +0200

    get today date in a different format

--- utils/date.ts
+++ utils/date.ts
@@ -1,3 +1,10 @@
-export function getToday() {
-  return new Date().toLocaleDateString();
+import dayjs from "dayjs";
+import { DEFAULT_DATE_FORMAT } from "@/config/date";
+
+export function getToday(format = DEFAULT_DATE_FORMAT) {
+  return dayjs().format(format);
+}
+
+export function getDisplayToday() {
+  return dayjs().format(DEFAULT_DATE_FORMAT);
 }
