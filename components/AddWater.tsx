1e48b9f27de2904b3e0ab1315e59ab9670a71b8c
Michal Staniecko <michal.staniecko@gmail.com>
2025-06-07 20:06:19 +0200

    improved press add-water button effect

--- components/AddWater.tsx
+++ components/AddWater.tsx
@@ -1,21 +1,30 @@
-import { Pressable, Text } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { useWater } from "@/hooks/useWater";
 import { useSetupStore } from "@/stores/setup";
 import { useTranslation } from "react-i18next";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function AddWater() {
   const { t } = useTranslation();
   const { addWater } = useWater();
   const { glassCapacity } = useSetupStore();
   return (
-    <Pressable onPress={addWater} className={"active:opacity-50"}>
-      <Text
-        className={
-          "bg-blue-500 border border-blue-500 text-white px-3 py-3 rounded text-center font-semibold"
-        }
+    <View
+      className={
+        "flex-row justify-stretch bg-blue-500 border border-blue-500 rounded"
+      }
+    >
+      <Pressable
+        onPress={addWater}
+        className={"flex-1 active:opacity-50 active:bg-blue-400 transition-all"}
       >
-        {t(`addWaterButton`, { capacity: glassCapacity })}
-      </Text>
-    </Pressable>
+        <Text className={" text-white px-3 py-3  text-center font-semibold"}>
+          {t(`addWaterButton`, { capacity: glassCapacity })}
+        </Text>
+      </Pressable>
+      <Pressable className={"border-l-2 border-blue-600 justify-center px-3"}>
+        <FontAwesome name={"edit"} size={16} color={"#ffffff"} />
+      </Pressable>
+    </View>
   );
 }
